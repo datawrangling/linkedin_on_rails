da9b91afdc0ef5b369433a8ae2497fb3e0a7fff9
Peter Skomoroch <pskomoro@pskomoro-mn.local>
2010-05-06 17:57:47 -0700

    fixed parsing for sparse profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,65 @@ class User < ActiveRecord::Base
   end
   
   
+  def extract_position(position)
+    # extract data
+    # create position record
+    # *** start and end dates may be missing ***           
+    position_params = {}
+    position_params.default = nil
+    position_params[:linkedin_position_id] = position['id']
+    position_params[:title] = position['title']
+    if position['summary']:
+      position_params[:summary] = position['summary']
+    end  
+    position_params[:is_current] = position['is-current']
+    if position['company']:
+      position_params[:company] = position['company']['name']   
+    end  
+    position_params[:start_date] = extract_date(position['start_date'])
+    position_params[:end_date] = extract_date(position['end_date'])    
+    return position_params
+  end
+  
+  def extract_education(education)
+    education_params = {}
+    education_params.default = nil          
+    education_params[:linkedin_education_id] = education['id']
+    education_params[:school_name] = education['school_name']
+    education_params[:degree] = education['degree'] 
+    education_params[:field_of_study] = education['field_of_study']
+    education_params[:notes] = education['notes'] 
+    education_params[:start_date] = extract_date(education['start_date'])
+    education_params[:end_date] = extract_date(education['end_date'])
+    return education_params
+  end
+  
+  def extract_connection(person)
+    # extract data
+    connection_params = {}
+    connection_params.default = nil
+    connection_params[:logged_in_url] = person['site_standard_profile_request']['url']
+    connection_params[:member_id] = connection_params[:logged_in_url].split('&')[1].split('=')[1].to_i
+    connection_params[:first_name] = person['first_name']
+    connection_params[:last_name] = person['last_name']
+    connection_params[:headline] = person['headline']
+    connection_params[:location] = person['location']['name']
+    connection_params[:location] = person['location']['country']['code']  
+    connection_params[:industry] = person['industry']                      
+    connection_params[:picture_url] = person['picture_url']
+    return connection_params
+  end
+  
+  def extract_member_urls(member_urls)
+    member_url_resources = nil
+    if member_urls['total'].to_i > 1    
+      member_url_resources = member_urls
+    elsif member_urls['total'].to_i == 1
+      member_url_resources = member_urls
+      member_url_resources["member_url"] = [member_urls["member_url"]]
+    end
+    return member_url_resources.to_json
+  end  
   
 private
 
@@ -69,8 +128,8 @@ private
         self.location = user_info['location']['name'] 
         self.country = user_info['location']['country']['code']
         self.honors = user_info['honors']
-        self.associations = user_info['associations']
-        self.member_url_resources = user_info['member_url_resources'].to_json                     
+        self.associations = user_info['associations']            
+        self.member_url_resources = extract_member_urls(user_info['member_url_resources'])                 
       end
       
     end  
@@ -78,74 +137,48 @@ private
       
   def populate_child_models 
     unless oauth_token.blank?     
-      # 2) Save past position info for user (companies, job titles, durations, descriptions)
       @response = UserSession.oauth_consumer.request(:get, 
       'http://api.linkedin.com/v1/people/~:(id,positions,educations,connections)',
       access_token, { :scheme => :query_string })
       case @response
       when Net::HTTPSuccess
-        user_info = Crack::XML.parse(@response.body)['person']        
-        user_info['positions']['position'].each do |position|
-          # create position record
-          # *** start and end dates may be missing ***         
-          # <position>
-          #   <id>88857582</id>
-          #   <title>Sr. Data Scientist</title>
-          #   <summary>Applying statistical approaches at scale at LinkedIn.  Mining information from LinkedIn profile content, the social graph, and external data sources to build data driven products and surface actionable insights for members.  My current tool set includes things like Hadoop, Pig, Hive, Voldemort, Mechanical Turk, Java, Python, NLTK, along with various machine learning and numerical libraries.  Building prototype web applications with Rails and Django.</summary>
-          #   <start-date>
-          #     <year>2009</year>
-          #     <month>9</month>
-          #   </start-date>
-          #   <is-current>true</is-current>
-          #   <company>
-          #     <name>LinkedIn</name>
-          #   </company>
-          # </position> 
-          position_params = {}
-          position_params[:linkedin_position_id] = position['id']
-          position_params[:title] = position['title']
-          position_params[:summary] = position['summary']
-          position_params[:is_current] = position['is-current']
-          position_params[:company] = position['company']['name']   
-          position_params[:start_date] = extract_date(position['start_date'])
-          position_params[:end_date] = extract_date(position['end_date']) 
-                     
-          self.positions.create(position_params)         
-        end  
+        user_info = Crack::XML.parse(@response.body)['person'] 
+        
+        # 2) Save past position info for user (companies, job titles, durations, descriptions) 
+        if user_info['positions']['total'].to_i > 1    
+          user_info['positions']['position'].each do |position|
+            position_params = extract_position(position)
+            self.positions.create(position_params)
+          end
+        else
+          position_params = extract_position(user_info['positions']['position'])
+          self.positions.create(position_params)
+        end 
 
-        # # 3) Save education info for user (schools, degrees, field of study, dates, etc)        
-        user_info['educations']['education'].each do |education|
-          # create education record
-          education_params = {}
-          education_params[:linkedin_education_id] = education['id']
-          education_params[:school_name] = education['school_name']
-          education_params[:degree] = education['degree'] 
-          education_params[:field_of_study] = education['field_of_study']
-          education_params[:notes] = education['notes'] 
-          education_params[:start_date] = extract_date(education['start_date'])
-          education_params[:end_date] = extract_date(education['end_date'])
-          
-          self.educations.create(education_params)     
-        end        
+        # 3) Save education info for user (schools, degrees, field of study, dates, etc)    
+        if user_info['educations']['total'].to_i > 1    
+          user_info['educations']['education'].each do |education|
+            education_params = extract_education(education)
+            self.educations.create(education_params)
+          end
+        else
+          education_params = extract_education(user_info['educations']['education'])
+          self.educations.create(education_params)
+        end    
 
         # 4) Save connections info - names, industries, headlines, profile pics,
         # Use Connections model
-        user_info['connections']['person'].each do |person|
-          #puts person['first_name'] + ' ' + person['last_name']
-          connection_params = {}
-          connection_params[:logged_in_url] = person['site_standard_profile_request']['url']
-          connection_params[:member_id] = connection_params[:logged_in_url].split('&')[1].split('=')[1].to_i
-          connection_params[:first_name] = person['first_name']
-          connection_params[:last_name] = person['last_name']
-          connection_params[:headline] = person['headline']
-          connection_params[:location] = person['location']['name']
-          connection_params[:location] = person['location']['country']['code']  
-          connection_params[:industry] = person['industry']                      
-          connection_params[:picture_url] = person['picture_url']                    
-          
+        if user_info['connections']['total'].to_i > 1    
+          user_info['connections']['person'].each do |person|
+            connection_params = extract_connection(person)
+            self.connections.create(connection_params)
+          end
+        else
+          puts user_info['connections'].inspect
+          connection_params = extract_connection(user_info['connections']['person'])
           self.connections.create(connection_params)
-          
-        end  
+        end        
+           
       end    
       
     end  
